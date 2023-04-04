a606f1c3d96e301ea8025b2173f032076832edb0
Annie Kuxa <kuxa.business@gmail.com>
2023-04-04 12:31:17 -0300

    add lines deletion after completion

--- js/game.js
+++ js/game.js
@@ -110,7 +110,7 @@ export default class Game {
     }
     
     piece.x = Math.floor((10 - piece.blocks[0].length) / 2)
-    piece.y = 0
+    piece.y = -1 
     return piece /*{
       x: 0,
       y: 0,
@@ -165,6 +165,7 @@ export default class Game {
     if (this.hasCollision()) {
       this.activePiece.y -= 1
       this.lockPiece()
+      this.clearLines()
       this.updatePIeces()
     }
   }
@@ -204,6 +205,35 @@ export default class Game {
           this.playfield[pieceY + y][pieceX + x] = blocks[y][x]
         }
       }
+    }
+  }
+
+  clearLines() {
+    const rows = 20
+    const columns = 10
+    let lines = []
+
+    for (let y =  rows - 1; y >= 0; y--) {
+        let numberOfBlocks = 0
+
+        for (let x = 0; x < columns; x++) {
+            if (this.playfield[y][x]) {
+              numberOfBlocks += 1
+            }
+        }
+
+        if (numberOfBlocks === 0) {
+          break
+        } else if (numberOfBlocks < columns) {
+          continue
+        } else if (numberOfBlocks === columns) {
+          lines.unshift(y)
+        }
+    }
+
+    for (const index of lines) {
+        this.playfield.splice(index, 1)
+        this.playfield.unshift(new Array(columns).fill(0))
     }
   }
 
