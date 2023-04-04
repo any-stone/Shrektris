e85eda0a41e200ccdd797d38b91f74598dae2006
Annie Kuxa <kuxa.business@gmail.com>
2023-04-04 12:18:46 -0300

    piece appears in the center of the playfield now

--- js/game.js
+++ js/game.js
@@ -46,7 +46,7 @@ export default class Game {
   createPiece() {
     const index = Math.floor(Math.random() * 7)
     const type = "IJLOSTZ"[index]
-    const piece = { x: 0, y: 0 }
+    const piece = { }
 
     switch (type) {
       case "I":
@@ -108,6 +108,9 @@ export default class Game {
         break
       default: new Error("Unknown piece")
     }
+    
+    piece.x = Math.floor((10 - piece.blocks[0].length) / 2)
+    piece.y = 0
     return piece /*{
       x: 0,
       y: 0,
