2057ebbe40f7b18e17c3ac28c8da1a8d926c37d7
Annie Kuxa <kuxa.business@gmail.com>
2023-04-04 11:06:35 -0300

    visual modfications of the pieces and playfield (stroke corrections

--- js/view.js
+++ js/view.js
@@ -32,13 +32,17 @@ export default class View {
         const block = line[x];
         
         if (block) {
-            this.context.fillStyle = "red"
-            this.context.strokeStyle = "black"
-            this.context.lineWidth = 2
-
-            this.context.fillRect(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight)
+            this.renderBlock(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight, "red")
         }
       }
     }
+  }
+  renderBlock(x, y, width, height, color) {
+    this.context.fillStyle = color
+    this.context.strokeStyle = "black"
+    this.context.lineWidth = 2
+
+    this.context.fillRect(x, y, width, height)
+    this.context.strokeRect(x, y, width, height)
   }
 }
